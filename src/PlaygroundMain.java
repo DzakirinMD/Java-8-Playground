df6e9e2c01cc0a2d1aaf5e2ad47f708b785a5fa7
Dzakirin Muhammad <dzakirinmd@gmail.com>
2022-03-23 17:23:51 +0800

    added few variable

--- src/PlaygroundMain.java
+++ src/PlaygroundMain.java
@@ -5,21 +5,26 @@ public class PlaygroundMain {
         // Standard out
         System.out.println("Hello World!");
 
-        /**
-        Java Variables
-        Variables are containers for storing data values.
+        /*
+          Java Variables
+                  Variables are containers for storing data values.
 
-                In Java, there are different types of variables, for example:
+                          In Java, there are different types of variables, for example:
 
-        String - stores text, such as "Hello". String values are surrounded by double quotes
-        int - stores integers (whole numbers), without decimals, such as 123 or -123
-        float - stores floating point numbers, with decimals, such as 19.99 or -19.99
-        char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
-        boolean - stores values with two states: true or false
+                  String - stores text, such as "Hello". String values are surrounded by double quotes
+                  int - stores integers (whole numbers), without decimals, such as 123 or -123
+                  float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+                  char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+                  boolean - stores values with two states: true or false
          */
 
-        String name = "Name Staring";
+        String name = "String Name";
+        int myNum = 15;
+        //If you don't want others (or yourself) to overwrite existing values, use the final keyword (this will declare the variable as "final" or "constant", which means unchangeable and read-only)
+        final int myConstantNum = 15;
+        //myConstantNum = 20;  // will generate an error: cannot assign a value to a final variable
 
+        System.out.println(name + " " + myNum + " " + myConstantNum);
 
     }
 }
